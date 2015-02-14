e9315e899f0d4763ebe1923284316d4ab3e0c68c
Davi Ferreira <contato@daviferreira.com>
2015-02-14 17:14:01 +0100

    Add .travis.yml and test script to package.json

--- spec/util.spec.js
+++ spec/util.spec.js
@@ -1,21 +1,26 @@
-describe('#getSelectionText', function () {
-  it('should return the element selected text', function () {
-    var div = document.createElement('div');
-    div.innerHTML = 'Hello, world!';
-    document.body.appendChild(div);
-    selectElementContents(div);
-    expect(getSelectionText(document)).toEqual(div.innerHTML);
-    document.body.removeChild(div);
-  });
-});
+describe('Util TestCase', function () {
+  describe('Selection', function () {
+    beforeEach(function () {
+      this.div = document.createElement('div');
+      this.div.innerHTML = 'Hello, world!';
+      document.body.appendChild(this.div);
+      selectElementContents(this.div);
+    });
 
-describe('#getSelectionStart', function () {
-  it('should return the selection node', function () {
-    var div = document.createElement('div');
-    div.innerHTML = 'Hello, world!';
-    document.body.appendChild(div);
-    selectElementContents(div);
-    expect(getSelectionStart(document)).toEqual(div);
-    document.body.removeChild(div);
-  });
+    afterEach(function () {
+      document.body.removeChild(this.div);
+    });
+
+    describe('#getSelectionText', function () {
+      it('should return the element selected text', function () {
+        expect(getSelectionText(document)).toEqual(this.div.innerHTML);
+      });
+    });
+
+    describe('#getSelectionStart', function () {
+      it('should return the selection node', function () {
+        expect(getSelectionStart(document)).toEqual(this.div);
+      });
+    });
+  })
 });
