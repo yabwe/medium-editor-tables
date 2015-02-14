03df5b73be391eabe1bafb9aa5ac9ccede7c9e54
Davi Ferreira <contato@daviferreira.com>
2015-02-14 02:26:42 +0100

    Transform prototype into an object

--- dist/js/medium-editor-tables.js
+++ dist/js/medium-editor-tables.js
@@ -8,6 +8,8 @@ if (typeof module === 'object') {
 }
 
 function MediumEditorTable() {
+  'use strict';
+
   this.parent = true;
   this.hasForm = true;
   this.showGrid = false;
@@ -94,237 +96,238 @@ function MediumEditorTable() {
     }
   }
 
+  MediumEditorTable.prototype = {
+    createButton: function () {
+      this.button = document.createElement('button');
+      this.button.className = 'medium-editor-action';
+      this.button.innerHTML = 'tbl';
+      this.button.addEventListener('click', function (e) {
+        e.preventDefault();
+        this.clickHandler();
+      }.bind(this));
+    },
 
-  MediumEditorTable.prototype.createButton = function () {
-    this.button = document.createElement('button');
-    this.button.className = 'medium-editor-action';
-    this.button.innerHTML = '<i class="table"></i>';
-    this.button.addEventListener('click', function (e) {
-      e.preventDefault();
-      this.clickHandler();
-    }.bind(this));
-  };
-
-  MediumEditorTable.prototype.clickHandler = function () {
-    if (this.showGrid === true) {
-      this.hide();
-    } else {
-      this.show();
-    }
-  };
-
-  MediumEditorTable.prototype.hide = function () {
-    this.showGrid = false;
-    this.tableBuilderElement.style.display = '';
-    this.button.classList.remove('medium-editor-button-active');
-  };
-
-  MediumEditorTable.prototype.show = function () {
-    this.showGrid = true;
-    this.tableBuilderElement.style.display = 'block';
-    this.tableBuilderElement.style.left = this.button.offsetLeft + 'px';
-    this.button.classList.add('medium-editor-button-active');
-  };
-
-  MediumEditorTable.prototype.getForm = function() {
-    if (!this.tableBuilderElement) {
-      this.createTableBuilder();
-    }
-
-    return this.tableBuilderElement;
-  };
-
-  MediumEditorTable.prototype.createTableBuilder = function () {
-    this.tableBuilderElement = this.base.options
-                                        .ownerDocument.createElement('div');
-    this.tableBuilderElement.className = 'medium-editor-table-builder';
-
-    this.renderGrid();
-
-    this.bind();
-  };
-
-  MediumEditorTable.prototype.generateCells = function () {
-    this.cells = [];
-
-    for (var i = 0; i < 100; i++) {
-      var col = i % 10;
-      var row = Math.floor(i/10);
-
-      this.cells.push({
-        col: col,
-        row: row,
-        active: false
-      });
-    }
-  };
-
-  MediumEditorTable.prototype.getGridHTML = function () {
-    var html = '<div class="medium-editor-table-builder-grid clearfix">';
-    html += this.getCellsHTML();
-    html += '</div>';
-    return html;
-  };
-
-  MediumEditorTable.prototype.getCellsHTML = function () {
-    var html = '';
-    this.generateCells();
-    this.cells.map(function(cell) {
-      html += '<a href="#" class="medium-editor-table-builder-cell' +
-              (cell.active === true ? ' active' : '') +
-              '" ' + 'data-row="' + cell.row +
-              '" data-col="' + cell.col + '">';
-      html += '</a>';
-    });
-    return html;
-  };
-
-  MediumEditorTable.prototype.renderGrid = function () {
-    this.tableBuilderElement.innerHTML = this.getGridHTML();
-    this.cellsElements = this.tableBuilderElement.querySelectorAll('a');
-  };
-
-  MediumEditorTable.prototype.onHide = function () {
-    this.hide();
-    this.hoveredCell = {
-      col: -1,
-      row: -1
-    };
-    this.markCells();
-  };
-
-  MediumEditorTable.prototype.getButton = function () {
-    if (this.base.options.buttonLabels === 'fontawesome') {
-      this.button.innerHTML = '<i class="fa fa-table"></i>';
-    }
-    return this.button;
-  };
-
-  MediumEditorTable.prototype.bind = function () {
-    [].forEach.call(this.cellsElements, function(el) {
-      this.bindMouseEnter(el);
-      this.bindClick(el);
-    }.bind(this));
-
-    this.bindTabOnTables();
-  };
-
-  MediumEditorTable.prototype.bindMouseEnter = function (el) {
-    var self = this;
-    var timer;
-
-    el.addEventListener('mouseenter', function () {
-      clearTimeout(timer);
-
-      var dataset = this.dataset;
-
-      timer = setTimeout(function () {
-        self.hoveredCell = {
-          col: parseInt(dataset.col, 10),
-          row: parseInt(dataset.row, 10)
-        };
-        console.log(self.hoveredCell);
-        self.markCells();
-      }, 10);
-    });
-  };
-
-  MediumEditorTable.prototype.bindClick = function (el) {
-    var self = this;
-    el.addEventListener('click', function (e) {
-      e.preventDefault();
-      self.insertTable(this.dataset.col, this.dataset.row);
-    });
-  };
-
-  MediumEditorTable.prototype.markCells = function () {
-    var self = this;
-    [].forEach.call(this.cellsElements, function(el) {
-      var cell = {
-        col: parseInt(el.dataset.col, 10),
-        row: parseInt(el.dataset.row, 10)
-      };
-      var active = self.hoveredCell &&
-                   cell.row <= self.hoveredCell.row  &&
-                   cell.col <= self.hoveredCell.col;
-      if (active === true) {
-        el.classList.add('active');
+    clickHandler: function () {
+      if (this.showGrid === true) {
+        this.hide();
       } else {
-        el.classList.remove('active');
+        this.show();
       }
-    });
-  };
+    },
 
-  MediumEditorTable.prototype.insertTable = function (cols, rows) {
-    var html = '';
-    var x;
-    var y;
+    hide: function () {
+      this.showGrid = false;
+      this.tableBuilderElement.style.display = '';
+      this.button.classList.remove('medium-editor-button-active');
+    },
 
-    for (y = 0; y <= rows; y++) {
-      html += '<tr>';
-      for (x = 0; x <= cols; x++) {
-        html += '<td><br /></td>';
+    show: function () {
+      this.showGrid = true;
+      this.tableBuilderElement.style.display = 'block';
+      this.tableBuilderElement.style.left = this.button.offsetLeft + 'px';
+      this.button.classList.add('medium-editor-button-active');
+    },
+
+    getForm: function() {
+      if (!this.tableBuilderElement) {
+        this.createTableBuilder();
       }
-      html += '</tr>';
-    }
 
-    this.base.insertHTML(
-      '<table class="medium-editor-table" id="medium-editor-table">' +
-      '<tbody>' +
-      html +
-      '</tbody>' +
-      '</table>' +
-      '<p><br /></p>'
-    );
+      return this.tableBuilderElement;
+    },
 
-    var table = this.base.options.ownerDocument
-                                 .getElementById('medium-editor-table');
-    table.removeAttribute('id');
-    placeCaretAtNode(table.querySelector('td'), true);
-  };
+    createTableBuilder: function () {
+      this.tableBuilderElement = this.base.options
+                                          .ownerDocument.createElement('div');
+      this.tableBuilderElement.className = 'medium-editor-table-builder';
 
-  MediumEditorTable.prototype.bindTabOnTables = function () {
-    var self = this;
+      this.renderGrid();
 
-    [].forEach.call(this.base.elements, function (el) {
-      el.addEventListener('keydown', function (e) {
-        var el = getSelectionStart(self.base.options.ownerDocument),
-            row = row,
-            table = table;
+      this.bind();
+    },
 
-        if (e.which === 9 && isInsideTable(el)) {
-          e.preventDefault();
-          e.stopPropagation();
-          el = getParentOf(el, 'td');
-          row = getParentOf(el, 'tr');
-          table = getParentOf(el, 'table');
-          if (e.shiftKey) {
-            placeCaretAtNode(
-              el.previousSibling || getPreviousRowLastCell(row),
-              true
-            );
-          } else {
-            if (isLastCell(el, row, table)) {
-              self.insertRow(
-                getParentOf(el, 'tbody'),
-                row.cells.length
-              );
-            }
-            placeCaretAtNode(el);
-          }
+    generateCells: function () {
+      this.cells = [];
+
+      for (var i = 0; i < 100; i++) {
+        var col = i % 10;
+        var row = Math.floor(i/10);
+
+        this.cells.push({
+          col: col,
+          row: row,
+          active: false
+        });
+      }
+    },
+
+    getGridHTML: function () {
+      var html = '<div class="medium-editor-table-builder-grid clearfix">';
+      html += this.getCellsHTML();
+      html += '</div>';
+      return html;
+    },
+
+    getCellsHTML: function () {
+      var html = '';
+      this.generateCells();
+      this.cells.map(function(cell) {
+        html += '<a href="#" class="medium-editor-table-builder-cell' +
+                (cell.active === true ? ' active' : '') +
+                '" ' + 'data-row="' + cell.row +
+                '" data-col="' + cell.col + '">';
+        html += '</a>';
+      });
+      return html;
+    },
+
+    renderGrid: function () {
+      this.tableBuilderElement.innerHTML = this.getGridHTML();
+      this.cellsElements = this.tableBuilderElement.querySelectorAll('a');
+    },
+
+    onHide: function () {
+      this.hide();
+      this.hoveredCell = {
+        col: -1,
+        row: -1
+      };
+      this.markCells();
+    },
+
+    getButton: function () {
+      if (this.base.options.buttonLabels === 'fontawesome') {
+        this.button.innerHTML = '<i class="fa fa-table"></i>';
+      }
+      return this.button;
+    },
+
+    bind: function () {
+      [].forEach.call(this.cellsElements, function(el) {
+        this.bindMouseEnter(el);
+        this.bindClick(el);
+      }.bind(this));
+
+      this.bindTabOnTables();
+    },
+
+    bindMouseEnter: function (el) {
+      var self = this;
+      var timer;
+
+      el.addEventListener('mouseenter', function () {
+        clearTimeout(timer);
+
+        var dataset = this.dataset;
+
+        timer = setTimeout(function () {
+          self.hoveredCell = {
+            col: parseInt(dataset.col, 10),
+            row: parseInt(dataset.row, 10)
+          };
+          console.log(self.hoveredCell);
+          self.markCells();
+        }, 10);
+      });
+    },
+
+    bindClick: function (el) {
+      var self = this;
+      el.addEventListener('click', function (e) {
+        e.preventDefault();
+        self.insertTable(this.dataset.col, this.dataset.row);
+      });
+    },
+
+    markCells: function () {
+      var self = this;
+      [].forEach.call(this.cellsElements, function(el) {
+        var cell = {
+          col: parseInt(el.dataset.col, 10),
+          row: parseInt(el.dataset.row, 10)
+        };
+        var active = self.hoveredCell &&
+                     cell.row <= self.hoveredCell.row  &&
+                     cell.col <= self.hoveredCell.col;
+        if (active === true) {
+          el.classList.add('active');
+        } else {
+          el.classList.remove('active');
         }
       });
-    });
-  };
+    },
 
-  MediumEditorTable.prototype.insertRow = function (tbody, cols) {
-    var tr = document.createElement('tr'),
-        html = '',
-        i;
-    for (i = 0; i < cols; i += 1) {
-      html += '<td><br /></td>';
+    insertTable: function (cols, rows) {
+      var html = '';
+      var x;
+      var y;
+
+      for (y = 0; y <= rows; y++) {
+        html += '<tr>';
+        for (x = 0; x <= cols; x++) {
+          html += '<td><br /></td>';
+        }
+        html += '</tr>';
+      }
+
+      this.base.insertHTML(
+        '<table class="medium-editor-table" id="medium-editor-table">' +
+        '<tbody>' +
+        html +
+        '</tbody>' +
+        '</table>' +
+        '<p><br /></p>'
+      );
+
+      var table = this.base.options.ownerDocument
+                                   .getElementById('medium-editor-table');
+      table.removeAttribute('id');
+      placeCaretAtNode(table.querySelector('td'), true);
+    },
+
+    bindTabOnTables: function () {
+      var self = this;
+
+      [].forEach.call(this.base.elements, function (el) {
+        el.addEventListener('keydown', function (e) {
+          var el = getSelectionStart(self.base.options.ownerDocument),
+              row = row,
+              table = table;
+
+          if (e.which === 9 && isInsideTable(el)) {
+            e.preventDefault();
+            e.stopPropagation();
+            el = getParentOf(el, 'td');
+            row = getParentOf(el, 'tr');
+            table = getParentOf(el, 'table');
+            if (e.shiftKey) {
+              placeCaretAtNode(
+                el.previousSibling || getPreviousRowLastCell(row),
+                true
+              );
+            } else {
+              if (isLastCell(el, row, table)) {
+                self.insertRow(
+                  getParentOf(el, 'tbody'),
+                  row.cells.length
+                );
+              }
+              placeCaretAtNode(el);
+            }
+          }
+        });
+      });
+    },
+
+    insertRow: function (tbody, cols) {
+      var tr = document.createElement('tr'),
+          html = '',
+          i;
+      for (i = 0; i < cols; i += 1) {
+        html += '<td><br /></td>';
+      }
+      tr.innerHTML = html;
+      tbody.appendChild(tr);
     }
-    tr.innerHTML = html;
-    tbody.appendChild(tr);
   };
 }(window, document));
