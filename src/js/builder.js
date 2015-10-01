e064d4085fa73c0efbc4dfbe918ad50952426cde
Gerardo <gerardo@cloudbits.com.ar>
2015-10-01 16:09:37 -0300

    Controls to add/remove rows/columns

--- src/js/builder.js
+++ src/js/builder.js
@@ -14,6 +14,48 @@ Builder.prototype = {
           this.options.rows,
           this.options.columns
         );
+
+        this._range = null;
+        this._toolbar = this._doc.createElement('div');
+        this._toolbar.className = 'medium-editor-table-builder-toolbar';
+
+        var addRowBefore = this._doc.createElement('button');
+        addRowBefore.title = 'Add row before';
+        addRowBefore.innerHTML = '<i class="fa fa-long-arrow-up"></i>';
+        addRowBefore.onclick = this.addRow.bind(this, true);
+        this._toolbar.appendChild(addRowBefore);
+
+        var addRowAfter = this._doc.createElement('button');
+        addRowAfter.title = 'Add row after';
+        addRowAfter.innerHTML = '<i class="fa fa-long-arrow-down"></i>';
+        addRowAfter.onclick = this.addRow.bind(this, false);
+        this._toolbar.appendChild(addRowAfter);
+
+        var remRow = this._doc.createElement('button');
+        remRow.title = 'Remove row';
+        remRow.innerHTML = '<i class="fa fa-close"></i>';
+        remRow.onclick = this.removeRow.bind(this);
+        this._toolbar.appendChild(remRow);
+
+        var addColumnBefore = this._doc.createElement('button');
+        addColumnBefore.title = 'Add column before';
+        addColumnBefore.innerHTML = '<i class="fa fa-long-arrow-left"></i>';
+        addColumnBefore.onclick = this.addColumn.bind(this, true);
+        this._toolbar.appendChild(addColumnBefore);
+
+        var addColumnAfter = this._doc.createElement('button');
+        addColumnAfter.title = 'Add column after';
+        addColumnAfter.innerHTML = '<i class="fa fa-long-arrow-right"></i>';
+        addColumnAfter.onclick = this.addColumn.bind(this, false);
+        this._toolbar.appendChild(addColumnAfter);
+
+        var remColumn = this._doc.createElement('button');
+        remColumn.title = 'Remove column';
+        remColumn.innerHTML = '<i class="fa fa-close"></i>';
+        remColumn.onclick = this.removeColumn.bind(this);
+        this._toolbar.appendChild(remColumn);
+
+        this._root.appendChild(this._toolbar);
     },
 
     getElement: function () {
@@ -29,5 +71,78 @@ Builder.prototype = {
     show: function (left) {
         this._root.style.display = 'block';
         this._root.style.left = left + 'px';
+    },
+
+    setEditor: function (range) {
+        this._range = range;
+        this._doc.getElementsByClassName('medium-editor-table-builder-toolbar')[0].style.display = 'block';
+    },
+
+    setBuilder: function () {
+        this._range = null;
+        this._doc.getElementsByClassName('medium-editor-table-builder-toolbar')[0].style.display = 'none';
+        var elements = this._doc.getElementsByClassName('medium-editor-table-builder-grid');
+        for (var i = 0; i < elements.length; i++) {
+            // TODO: what is 16 and what is 2?
+            elements[i].style.height = (16 * this.rows + 2) + 'px';
+            elements[i].style.width = (16 * this.columns + 2) + 'px';
+        }
+    },
+
+    addRow: function (before, e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var tbody = this._range.parentNode.parentNode,
+            tr = this._doc.createElement('tr');
+        for (var i = 0; i < this._range.parentNode.childNodes.length; i++) {
+            tr.appendChild(this._doc.createElement('td'));
+        }
+        if (before !== true && this._range.parentNode.nextSibling) {
+            tbody.insertBefore(tr, this._range.parentNode.nextSibling);
+        } else if (before === true) {
+            tbody.insertBefore(tr, this._range.parentNode);
+        } else {
+            tbody.appendChild(tr);
+        }
+        this.options.onClick(0, 0);
+    },
+
+    removeRow: function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        this._range.parentNode.parentNode.removeChild(this._range.parentNode);
+        this.options.onClick(0, 0);
+    },
+
+    addColumn: function (before, e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var cell = Array.prototype.indexOf.call(this._range.parentNode.childNodes, this._range),
+            tbody = this._range.parentNode.parentNode;
+
+        for (var i = 0; i < tbody.childNodes.length; i++) {
+            if (before === true) {
+                tbody.childNodes[i].insertBefore(this._doc.createElement('td'), tbody.childNodes[i].childNodes[cell]);
+            } else if (this._range.parentNode.parentNode.childNodes[i].childNodes[cell].nextSibling) {
+                tbody.childNodes[i].insertBefore(this._doc.createElement('td'), tbody.childNodes[i].childNodes[cell].nextSibling);
+            } else {
+                tbody.childNodes[i].appendChild(this._doc.createElement('td'));
+            }
+        }
+
+        this.options.onClick(0, 0);
+    },
+
+    removeColumn: function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var cell = Array.prototype.indexOf.call(this._range.parentNode.childNodes, this._range),
+            tbody = this._range.parentNode.parentNode,
+            rows = tbody.childNodes.length;
+
+        for (var i = 0; i < rows; i++) {
+            tbody.childNodes[i].removeChild(tbody.childNodes[i].childNodes[cell]);
+        }
+        this.options.onClick(0, 0);
     }
 };
